58aec5bdcbe1d8aee64784d1caccaab593fb5b9b
Brian <mahuibrian14@gmail.com>
2024-01-19 12:40:50 +0300

    work in progress

--- libs/features/convs-mgr/stories/home/src/lib/components/bots/bots-list-latest-courses/bots-list-latest-courses.component.ts
+++ libs/features/convs-mgr/stories/home/src/lib/components/bots/bots-list-latest-courses/bots-list-latest-courses.component.ts
@@ -10,11 +10,14 @@ import { __DateFromStorage } from '@iote/time';
 import { Bot } from '@app/model/convs-mgr/bots';
 
 //import { ActivatedRoute } from '@angular/router'; //to get the botId
+import { catchError } from 'rxjs/operators';
+import { EMPTY } from 'rxjs';
 import { forkJoin } from 'rxjs';
 import { BotModule } from '@app/model/convs-mgr/bot-modules';//to access stories related to the bot
 import { BotModulesStateService } from '@app/state/convs-mgr/modules';
 import { StoryStateService } from '@app/state/convs-mgr/stories';
 import { Story } from '@app/model/convs-mgr/stories/main';
+import { BotsStore } from 'libs/state/convs-mgr/bots/src/lib/stores/bots.store';
 
 import { MatDialog } from '@angular/material/dialog';
 
@@ -38,95 +41,56 @@ export class BotsListLatestCoursesComponent implements OnInit {
   
   bots: Bot[];
 
+  botId:string | undefined;
+
   screenWidth: number;
 
   constructor(private _router$$: Router,
               private _dialog: MatDialog,
               private botModulesService: BotModulesStateService , //added 
               private storyStateService: StoryStateService,
+              private _BotsStore: BotsStore,
             //  private route: ActivatedRoute // Inject ActivatedRoute                      
     ) {}
 
   ngOnInit(): void {
   this.screenWidth = window.innerWidth;
+  modules:BotModule[];
      
-
-    // if (this.bots$) {
-    //   // console.log(`Here are the bots:`,bots);
-    //   this.bots$.pipe(
-    //     map((s) => __orderBy(s,(a) => __DateFromStorage(a.createdOn as Date).unix(), 'desc')),
-    //     tap((s) => this.bots = s),
-    //     map((bots) => bots.map(bot => bot.id)),
-    //     tap((botIds) => {
-    //         console.log('Here are the botIds:', botIds);})
-    //     ).subscribe();
-    //     console.log(`Here are the bots:`,this.bots);
-    // }
-
-    // this.modules$ = this.botModulesService.getBotModulesFromParentBot(botId);
-    // this.modules$.subscribe(modules => {
-    //   if (modules && modules.length > 0) {
-    //   console.log('Modules emitted:', modules);
-    //   this.modules = modules;
-    //       }else {
-    //        console.log('No module data');
-    //       }
-    // });
-    // this.stories$ = this.storyStateService.getStoriesFromParentModule(moduleId: string);
-    // this.stories$.subscribe(stories => {
-    //   if (stories && stories.length > 0) {
-    //   console.log('Stories emitted:', stories);
-    //   this.stories = stories;
-    //       }else {
-    //        console.log('No Stories data');
-    //       }
-    // });
-
-    let botIds: string[] = [];
-
-    if (this.bots$) {
-      this.bots$.pipe(
-        map((s) => __orderBy(s, (a) => __DateFromStorage(a.createdOn as Date).unix(), 'desc')),
-        tap((s) => this.bots = s),
-        map((bots) => bots.map(bot => bot.id)),
-        tap((resultBotIds) => {
-          // Filter out undefined or null values
-          botIds = resultBotIds.filter(id => id !== undefined && id !== null) as string[];
-          console.log('Here are the botIds:', botIds);
-        })
-      ).subscribe(() => {
-        // Inside the subscribe block to ensure asynchronous completion
-    
-        // Access botIds outside the observable chain
-        console.log('Access botIds outside the observable chain:', botIds);
-    
-        // Use the first botId or handle multiple botIds as needed
-        const firstBotId = botIds.length > 0 ? botIds[0] : null;
-    
-        // Use the botId in the modules block
-        if (firstBotId) {
-          this.modules$ = this.botModulesService.getBotModulesFromParentBot(firstBotId);
-          this.modules$.subscribe(modules => {
-            if (modules && modules.length > 0) {
-              console.log('Modules emitted:', modules);
-              this.modules = modules;
-            } else {
-              console.log('No module data');
-            }
-          });
-        } else {
-          console.log('No valid botId to fetch modules.');
-        }
-      });
-    
-      // Note: This console.log will execute before the asynchronous operations within the observable chain.
-      console.log(`Here are the bots:`, this.bots);
-    }
-
-
-
-    
+  if (this.bots$) {
+    this.bots$.pipe(
+      map((s) => __orderBy(s, (a) => __DateFromStorage(a.createdOn as Date).unix(), 'desc')),
+      tap((s) => {
+        this.bots = s;
+        console.log('Here are the bots:', this.bots);
+      }),
+      map((bots) => bots.map(bot => bot.id)),
+      tap((botIds) => {
+      //  this.botId = botIds
+        console.log('Here are the botIds:', botIds);
+      })
+    ).subscribe(
+      
+    );
   }
+
+    this.modules$ = this.botModulesService.getBotModules();
+    this.modules$.subscribe(modules => {
+      if (modules && modules.length > 0) {
+      console.log('Modules emitted:', modules);
+      this.modules = modules;
+          }else {
+           console.log('No module data');
+          }
+    });
+
+    if(modules )
+
+    
+  
+  }
+
+
   
   openBot(id: string) {
     console.log("openBot div clicked");
@@ -135,6 +99,6 @@ export class BotsListLatestCoursesComponent implements OnInit {
   openMainStory(id: string) {
     this._router$$.navigate(['stories', id]);
   }
-}
 
- 
+
+}
